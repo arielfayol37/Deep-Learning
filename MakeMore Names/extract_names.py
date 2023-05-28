493b928e80b8ab25ef198e75e1f7c113703540bb
Fayol Zeudom <115123676+arielfayol37@users.noreply.github.com>
2023-05-27 19:42:05 -0700

    Update extract_names.py

--- MakeMore Names/extract_names.py
+++ MakeMore Names/extract_names.py
@@ -1,12 +1,12 @@
-# Extacting the names from a pdf file
-#import os
-#os.chdir(r'C:\Users\HP\Downloads')
+#Extacting the names from a pdf file
+import os
+os.chdir(r'C:\Users\Noella\Downloads')
 import PyPDF2 as pf
 import re
 pdfFileObj  = open('2021-GCE-AL-Results.pdf', 'rb')
 pdfReader = pf.PdfReader(pdfFileObj)
 # Regex to select the lines with the names and removing numbers
-pattern = re.compile(r'\(\d+\)\n([A-Z\s-]+)')
+pattern = re.compile(r'\(?\d+\)?\n([A-Z\s-]+)')
 
 lines_list = []
 for i in range(len(pdfReader.pages)):
@@ -18,10 +18,17 @@ for line in lines_list:
         names.extend(string.split(" "))
 
 new_names = []
-for name in reversed(names):
+for name in names:
     if (len(name) == 5 or (len(name) == 6 and name[0] == '\n')) and name[-2] == "-":
         continue
-    new_names.append(name)
+    if name == "-" or name=='\n':
+        continue
+    if len(name) > 0:
+        if name[0] == "-":
+            name = name[1:]
+        if name[-1] == "-":
+            name = name[:-1]
+        new_names.append(name.strip())
 new_names = list(set(new_names))
 new_names.sort()
 with open('cameroon_names.txt', 'w') as file:
