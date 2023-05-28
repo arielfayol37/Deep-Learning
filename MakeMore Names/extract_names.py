88b15d852b88fa743890a8dd581b9211dd44bf6c
Fayol Zeudom <115123676+arielfayol37@users.noreply.github.com>
2023-05-27 22:03:21 -0400

    Update extract_names.py
    Sorting the names before writing into a file

--- MakeMore Names/extract_names.py
+++ MakeMore Names/extract_names.py
@@ -3,8 +3,7 @@
 #os.chdir(r'C:\Users\HP\Downloads')
 import PyPDF2 as pf
 import re
-name_of_pdf_file = '2021-GCE-AL-Results.pdf'
-pdfFileObj  = open(name_of_pdf_file, 'rb')
+pdfFileObj  = open('2021-GCE-AL-Results.pdf', 'rb')
 pdfReader = pf.PdfReader(pdfFileObj)
 # Regex to select the lines with the names and removing numbers
 pattern = re.compile(r'\(\d+\)\n([A-Z\s-]+)')
@@ -12,17 +11,20 @@ pattern = re.compile(r'\(\d+\)\n([A-Z\s-]+)')
 lines_list = []
 for i in range(len(pdfReader.pages)):
     lines_list.append(pattern.findall(pdfReader.pages[i].extract_text()))
-
+assert len(lines_list) > 0
 names = [] # some items in this list will be subjects and grades.
 for line in lines_list:
     for string in line:
         names.extend(string.split(" "))
 
-for index, name in enumerate(names):
-    if (len(name) == 4 or (len(name)==6 and name[0] == '\n')) and name[-2] == "-":
-        del names[index]
-names = list(set(names))        
+new_names = []
+for name in reversed(names):
+    if (len(name) == 5 or (len(name) == 6 and name[0] == '\n')) and name[-2] == "-":
+        continue
+    new_names.append(name)
+new_names = list(set(new_names))
+new_names.sort()
 with open('cameroon_names.txt', 'w') as file:
     # Write each name to a separate line in the file
-    for name in names:
+    for name in new_names:
         file.write(name.strip() + '\n')
